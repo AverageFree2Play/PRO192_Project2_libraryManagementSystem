6794fcff6c9b86aa50f740a5f8c30124f7537932
Baihuucaoco <155862492+Baihuucaoco@users.noreply.github.com>
2026-06-03 06:56:25 +0700

    .
    Co-Authored-By: Avery <124472904+AverageFree2Play@users.noreply.github.com>

--- LibraryManagement/src/MainProgram/Main.java
+++ LibraryManagement/src/MainProgram/Main.java
@@ -9,7 +9,9 @@ import ManagementObject.BookManagement;
 import ManagementObject.BorrowManagement;
 //import java.io.DataInput;
 import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         BookManagement bookMgmt = new BookManagement();
@@ -17,42 +19,42 @@ public class Main {
         boolean run = true;
         int choice;
         System.out.println("**********************");
-        System.out.println("LIBRARY MANAGEMENT SYSTEM");
         
-            do {
-                
-                System.out.println("**********************");
-                System.out.println("Welcome back!");
-                System.out.println("1.Manage Books\n2.Manage Members\n3.Borrowing/Returning\n4.Reports\n5.Exit");
-                System.out.println("----------------------");
-                System.out.println("Choose an option: ");
-                choice = sc.nextInt();
-                
-                switch (choice) {
-                    case 1 :                                           
-                        bookMgmt.bookManage(); break;
-                    case 2 :                       
-                        System.out.println("You chose Member Management\n");
-                    break;
-                    case 3 :
-                        brwMgmt.borrowManage();
-                    break;
-                    case 4 :
-                        System.out.println("You chose Reporting\n");
-                    break;
-                    case 5 :
-                        System.out.println("Good bye!\n");
-                        run = false;
-                    break;
-                    default :
-                        System.out.println("Invalid choice. Please try again!\n\n");
-                }
-            } while (run);
-            sc.close();
-        }
 
-        public void main() {
-            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-        }
+        do {
+            System.out.println("LIBRARY MANAGEMENT SYSTEM");
+            System.out.println("**********************");
+            System.out.println("Welcome back!");
+            System.out.println("1.Manage Books\n2.Manage Members\n3.Borrowing/Returning\n4.Reports\n5.Exit");
+            System.out.println("----------------------");
+            System.out.println("Choose an option: ");
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    bookMgmt.bookManage();
+                    break;
+                case 2:
+                    System.out.println("You chose Member Management\n");
+                    break;
+                case 3:
+                    brwMgmt.borrowManage();
+                    break;
+                case 4:
+                    System.out.println("You chose Reporting\n");
+                    break;
+                case 5:
+                    System.out.println("Good bye!\n");
+                    run = false;
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again!\n\n");
+            }
+        } while (run);
+        sc.close();
     }
-    
+
+    public void main() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
