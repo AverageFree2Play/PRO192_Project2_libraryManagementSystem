de2b9b754dfe5c8a124feab92261a8ab586e454c
Avery <kernel330@gmail.com>
2026-06-02 20:13:02 +0700

    add essential functions

--- LibraryManagement/src/Utilities/Menu.java
+++ LibraryManagement/src/Utilities/Menu.java
@@ -9,19 +9,30 @@ import java.util.List;
  * @author MSI'
  */
 public class Menu {
+    public static void print(String str) {
+        List<String> menuList = Arrays.asList(str.split("\\|"));
+        menuList.forEach(menuItem -> {
+            if (menuItem.equalsIgnoreCase("Select")) {
+                System.out.print(menuItem);
+            } else {
+                System.out.println(menuItem);
+            }
+
+        });
+    }
+
+    public static int getUserChoice() {
+        int number = 0;
+        try {
+            number = DataInput.getIntegerNumber();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return number;
+    }
     
-//    public static void printMenu(String st) {
-//        List<String> menuList = Array.asList(st.split("\\|"));
-//        menuList.forEach(menuItem -> {
-//            if (menuItem.equalsIgnoreCase("Select")){
-//                System.out.print(menuItem);
-//            }else{
-//                System.out.println(menuItem);
-//            }
-//        });
-//    }
-      public static void manageBook(){
-          BookManagement bmMenu = new BookManagement();
-      }
+    public static void manageBook(){
+        BookManagement bmMenu = new BookManagement();
+    }
    
 }
